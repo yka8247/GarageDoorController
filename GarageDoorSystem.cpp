28a5ada6ae5f1c408edd8159e0eceb39c1a5a024
Yongki JayZ An <yxa8247@rit.edu>
2018-02-26 16:45:43 -0500

    work under progress

--- GarageDoorSystem.cpp
+++ GarageDoorSystem.cpp
@@ -2,31 +2,72 @@
 #include <iostream>
 #include <pthread.h>
 #include <unistd.h>
+#include <sys/neutrino.h>
+#include <string.h>
 #include "GarageDoor.h"
-#include "mailbox.h"
+//#include "mailbox.h"
 
 
 void* GarageDoorThread( void* arg) {
-	//TODO: Add GarageDoor functionality here
+	int rcvid;
+	char message[512];
+	int chid = (int)arg;
 	GarageDoor GD;
+
+	//chid = ChannelCreate (0);
+
 	GD.Init();
 	while (TRUE) {
 		GarageDoorData* data = new GarageDoorData();
 		data->button_pushed = TRUE;
 		GD.Operate(data);
+
+		//read
+		rcvid = MsgReceive (chid, message, sizeof (message), NULL);
+		std::cout << "Receive ID: " << rcvid << std::endl;
+		std::cout << "Message: " << message << std::endl;
+		//reply
+		strcpy(message, "gg");
+		MsgReply(rcvid, 0, message, sizeof(message));
+
+		delay(500);
+	}
+	return (0);
+}
+
+void* MockSender( void* arg) {
+	int coid;
+	int chid = (int)arg;
+	const char* smsg = "hmm";
+	char rmsg [200];
+	coid = ConnectAttach(0,77,chid,0,0);
+
+	if (coid == -1) {
+		std::cout << "Connection Failed!" << std::endl;
+	}
+
+	if (MsgSend(coid, smsg, strlen(smsg) + 1, rmsg, sizeof (rmsg)) == -1 ){
+		std::cout << "Error during MsgSend" << std::endl;
+	}
+
+	if (strlen(rmsg) > 0) {
+		std::cout << "Process returns " << rmsg << std::endl;
 	}
 	return (0);
 }
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
+	int chid = ChannelCreate (0);
+
 	std::cout << "Initializing the Garage Door Simulation" << std::endl;
 	/* Prepare thread creation*/
 	pthread_attr_t attr;
 	pthread_attr_init( &attr );
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 	/* Thread Ready? */
-	pthread_create(NULL, &attr, &GarageDoorThread, NULL);
+	pthread_create(NULL, &attr, &GarageDoorThread, (void*)chid);
+	pthread_create(NULL, &attr, &MockSender, (void*)chid);
 	/* Run thread for 10 sec */
 	sleep(10);
 
--- MailBox.h
+++ MailBox.h
@@ -1,27 +1,27 @@
-/*
- * MailBox.h
- *
- *  Created on: Feb 24, 2018
- *      Author: yxa8247
- */
-
-#ifndef MAILBOX_H_
-#define MAILBOX_H_
-#include <queue.h>
-#include <pthread.h>
-#include "StateMachine.h"
-
-class MailBox {
-private:
-	pthread_mutex_t enq_mutex;
-	pthread_mutex_t deq_mutex;
-	pthread_cond_t cond;
-public:
-	void enqueue(const EventData& data);
-	void dequeue();
-	BOOL empty();
-	EventData& front();
-
-};
-
-#endif /* MAILBOX_H_ */
+///*
+// * MailBox.h
+// *
+// *  Created on: Feb 24, 2018
+// *      Author: yxa8247
+// */
+//
+//#ifndef MAILBOX_H_
+//#define MAILBOX_H_
+//#include <queue.h>
+//#include <pthread.h>
+//#include "StateMachine.h"
+//
+//class MailBox {
+//private:
+//	pthread_mutex_t enq_mutex;
+//	pthread_mutex_t deq_mutex;
+//	pthread_cond_t cond;
+//public:
+//	void enqueue(const EventData& data);
+//	void dequeue();
+//	BOOL empty();
+//	EventData& front();
+//
+//};
+//
+//#endif /* MAILBOX_H_ */
