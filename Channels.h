fab1f0ef925b51a8147747fb3d9aeb5f7af04541
Yongki JayZ An <yxa8247@rit.edu>
2018-03-13 16:54:22 -0400

    fixed messaging system

---
+++ Channels.h
@@ -0,0 +1,16 @@
+/*
+ * Channels.h
+ *
+ *  Created on: Mar 13, 2018
+ *      Author: Yongki An
+ */
+
+#ifndef CHANNELS_H_
+#define CHANNELS_H_
+
+struct Channels {
+	int is_chid;
+	int gdc_chid;
+};
+
+#endif /* CHANNELS_H_ */
--- GarageDoorSystem.cpp
+++ GarageDoorSystem.cpp
@@ -7,81 +7,141 @@
 #include <process.h>
 #include "GarageDoor.h"
 #include "InputScanner.h"
+#include "Channels.h"
 
 
-void* InputScannerThread( void* arg) {
-	char char_input;
-	//char* smsg;
-	int coid;
-	int chid = (int)arg;
-	char rmsg [50];
+void* InputScannerController( void* args) {
+	/* Initialize Message Channels */
+	Channels* chids = (Channels*)args;
+	int sendCh = chids->gdc_chid;
+	int receiveCh = chids->is_chid;
+	/* Initialize variables */
 	InputScanner IS;
-	coid = ConnectAttach(0,getpid(),chid,0,0);
-
-	std::cout << "Currently the simulation takes only keyboard input" << std::endl;
-	std::cout << "Press 'b' or 'B' to simulate Push_Button Effect" << std::endl;
-	std::cout << "Press 'o' or 'O' to simulate Overcurrent Effect" << std::endl;
-	std::cout << "Press 'i' or 'I' to simulate Infrared Beam Triggered Effect" << std::endl;
-
+	int coid;
+	int rcvid;
+	char rmsg [100];
+	char message[100];
+	coid = ConnectAttach(0,getpid(),sendCh,0,0);
+	if (coid == -1) {
+		std::cout << "Connection Failed" << std::endl;
+		return 0;
+	}
 
 	while (TRUE) {
-		std::cout << "Your input: ";
-		std::cin >> char_input;
-
-		if (char_input == 'b' || char_input == 'B') {
-			GarageDoorData* data = new GarageDoorData();
-			data->button_pushed = TRUE;
-			IS.Event_Received(data);
-			//strcpy(smsg, "button_pushed");
-			MsgSend(coid, (void*)data, sizeof(data), rmsg, sizeof (rmsg));
+		/* check if any message arrived */
+		rcvid = MsgReceive(receiveCh, message, sizeof(message), NULL);
+		if (rcvid) {
+			std::cout << "Arrived message <<Input Scanner>>: " << message << std::endl;
 		}
-		if (char_input == 'o' || char_input == 'O') {
-			GarageDoorData* data = new GarageDoorData();
-			data->overcurrent = TRUE;
-			IS.Event_Received(data);
-			//strcpy(smsg, "overcurrent");
-			MsgSend(coid, (void*)data, sizeof(data), rmsg, sizeof (rmsg));
-		}if (char_input == 'i' || char_input == 'I') {
-			GarageDoorData* data = new GarageDoorData();
-			data->ir_interrupt = TRUE;
-			IS.Event_Received(data);
-			//strcpy(smsg, "ir_interrupt");
-			MsgSend(coid, (void*)data, sizeof(data), rmsg, sizeof (rmsg));
+		/* send a response back to the sender */
+		std::string response = "InputScanner <<Received>>";
+		MsgReply(rcvid, 1, &response, sizeof(response));
+
+		/* forward to controller */
+		if(MsgSend(coid, message, strlen(message) + 1, rmsg, sizeof(rmsg)) == -1) {
+			std::cout << "Could not send message :[ " << message << std::endl;
 		}
-
-
-//		if (coid == -1) {
-//			std::cout << "Connection Failed!" << std::endl;
+		/* Terminate */
+		if(message[0] == 't' || message[0] == 'T'){
+			std::cout <<"Shut Down::<<InputScanner>>" << std::endl;
+			break;
+		}
+//		if (char_input == 'b' || char_input == 'B') {
+//			GarageDoorData* data = new GarageDoorData();
+//			data->button_pushed = TRUE;
+//			IS.Event_Received(data);
+//			//strcpy(smsg, "button_pushed");
+//			MsgSend(coid, (void*)data, sizeof(data), rmsg, sizeof (rmsg));
 //		}
-//		if (MsgSend(coid, (void*)data, strlen(smsg) + 1, rmsg, sizeof (rmsg)) == -1 ){
-//			std::cout << "Error during MsgSend" << std::endl;
-//		}
-//		if (strlen(rmsg) > 0) {
-//			std::cout << "Process returns " << rmsg << std::endl;
-//		}
-
-		delay(100);
 	}
 	return (0);
 }
 
 
-void* GarageDoorThread( void* arg) {
-	int rcvid;
-	char message[512];
-	GarageDoorData data;
-	int chid = (int)arg;
-	GarageDoor GD;
+void* KeyboardScannerController(void* args){
+	/* Initialize Message Channels */
+	Channels* chids = (Channels*)args;
+	int chid = chids->is_chid;
+	/* Initialize variables */
+	int coid;
+	char inp;
+	char rmsg[100];
+	coid = ConnectAttach(0, getpid(), chid, 0, 0);
+	if (coid == -1) {
+		std::cout << "Connection Failed" << std::endl;
+		return 0;
+	}
+	/* Print instruction messages for the keyboard simulation */
+	std::cout << "Initializing Keyboard Interrupt Simulation" << std::endl;
+	std::cout << "Press 'b' or 'B' to simulate Push_Button Effect" << std::endl;
+	std::cout << "Press 'o' or 'O' to simulate OverCurrent Effect" << std::endl;
+	std::cout << "Press 'i' or 'I' to simulate Infrared Beam Triggered Effect" << std::endl;
+	std::cout << "Press 't' or 'T' to Terminate the Simulation" << std::endl;
 
-	//chid = ChannelCreate (0);
+	while(1) {
+		std::cin >> inp;
+		switch(inp) {
+		case 'B':
+		case 'b': {
+			if(MsgSend(coid, &inp, strlen(&inp) + 1, rmsg, sizeof(rmsg)) == -1) {
+				std::cout << "Failed to send a message :: " << inp << std::endl;
+			}
+			break;
+		}
+		case 'I':
+		case 'i': {
+			if(MsgSend(coid, &inp, strlen(&inp) + 1, rmsg, sizeof(rmsg)) == -1) {
+				std::cout << "Failed to send a message :: " << inp << std::endl;
+			}
+			break;
+		}
+		case 'O':
+		case 'o': {
+			if(MsgSend(coid, &inp, strlen(&inp) + 1, rmsg, sizeof(rmsg)) == -1) {
+				std::cout << "Failed to send a message :: " << inp << std::endl;
+			}
+			break;
+		}
+		case 'T':
+		case 't': {
+			if(MsgSend(coid, &inp, strlen(&inp) + 1, rmsg, sizeof(rmsg)) == -1) {
+				std::cout << "Failed to send a message :: " << inp << std::endl;
+			}
+			std::cout << "Shutting down keyboard scanner..." << std::endl;
+			return EXIT_SUCCESS;
+		}
+		default:
+			std::cout << "Invalid input" << std::endl;
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
+
+
+void* GarageDoorController( void* args) {
+	/* Initialize Message Channels */
+	Channels* chids = (Channels*)args;
+	int sendCh = chids->is_chid;
+	int receiveCh = chids->gdc_chid;
+	/* Initialization */
+	//GarageDoorData* data;
+	//GarageDoor GD;
+	int coid;
+	int rcvid;
+	char message[100];
+	coid = ConnectAttach(0,getpid(),sendCh,0,0);
+	if (coid == -1) {
+		std::cout << "Connection Failed" << std::endl;
+		return 0;
+	}
 
 	while (TRUE) {
-		GarageDoorData* data = new GarageDoorData();
-
-		//read
-		rcvid = MsgReceive (chid, data, sizeof (data), NULL);
-		std::cout << "Receive ID: " << rcvid << std::endl;
-		//std::cout << "Message: " << message << std::endl;
+		// check if any message arrived
+		rcvid = MsgReceive (receiveCh, message, sizeof(message), NULL);
+		if (rcvid) {
+			std::cout << "Arrived message <<GDC>>: " << message << std::endl;
+		}
 //		if (message == "button_pushed") {
 //			data->button_pushed = TRUE;
 //			GD.Operate(data);
@@ -94,54 +154,67 @@ void* GarageDoorThread( void* arg) {
 //			data->ir_interrupt = TRUE;
 //			GD.Halt(data);
 //		}
-		GD.Operate(data);
-		//reply
-		strcpy(message, "<<Applied>>");
-		MsgReply(rcvid, 0, message, sizeof(message));
-
-		delay(100); // MsgReceive waits for this function
+//		GD.Operate(data);
+		/* send a response back to the sender */
+		std::string response = "<<GDC>> Received";
+		MsgReply(rcvid, 1, &response, sizeof(response));
+		/* Terminate */
+		if(message[0] == 't' || message[0] == 'T'){
+			std::cout <<"Shut Down::<<GDC>>" << std::endl;
+			break;
+		}
 	}
 	return (0);
 }
 
-void* MockSender( void* arg) {
-	int coid;
-	int chid = (int)arg;
-	const char* smsg = "hmm";
-	char rmsg [200];
-	coid = ConnectAttach(0,getpid(),chid,0,0);
-	while(TRUE) {
-		if (coid == -1) {
-			std::cout << "Connection Failed!" << std::endl;
-		}
-
-		if (MsgSend(coid, smsg, strlen(smsg) + 1, rmsg, sizeof (rmsg)) == -1 ){
-			std::cout << "Error during MsgSend" << std::endl;
-		}
-
-		if (strlen(rmsg) > 0) {
-			std::cout << "Process returns " << rmsg << std::endl;
-		}
-		delay(100);
-	}
-	return (0);
-}
+//void* MockSender( void* arg) {
+//	int coid;
+//	int chid = (int)arg;
+//	const char* smsg = "hmm";
+//	char rmsg [200];
+//	coid = ConnectAttach(0,getpid(),chid,0,0);
+//	while(TRUE) {
+//		if (coid == -1) {
+//			std::cout << "Connection Failed!" << std::endl;
+//		}
+//
+//		if (MsgSend(coid, smsg, strlen(smsg) + 1, rmsg, sizeof (rmsg)) == -1 ){
+//			std::cout << "Error during MsgSend" << std::endl;
+//		}
+//
+//		if (strlen(rmsg) > 0) {
+//			std::cout << "Process returns " << rmsg << std::endl;
+//		}
+//		delay(100);
+//	}
+//	return (0);
+//}
 
 
 int main(int argc, char **argv) {
-	int chid = ChannelCreate (0);
-
-	std::cout << "Initializing the Garage Door Simulation" << std::endl;
+	/* Get Full Permission over RTOS */
+	if ( ThreadCtl(_NTO_TCTL_IO, NULL) == -1) {
+		std::perror("Failed to get I/O access permission");
+		return 1;
+	}
 	/* Prepare thread creation*/
-	pthread_attr_t attr;
-	pthread_attr_init( &attr );
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-	/* Thread Ready? */
-	pthread_create(NULL, &attr, &GarageDoorThread, (void*)chid);
-	pthread_create(NULL, &attr, &InputScannerThread, (void*)chid);
-	/* Run thread for 600 sec */
-	sleep(600);
-
+	pthread_t keyboardScannerThread;
+	pthread_t inputScannerThread;
+	pthread_t GarageDoorThread;
+	/* Prepare channel structure creation */
+	int gdc_chid = ChannelCreate(0);
+	int is_chid = ChannelCreate(0);
+	Channels channels = { is_chid, gdc_chid };
+	/* Initialization complete -> Move on to runtime environment */
+	std::cout << "Initializing the Garage Door Simulation" << std::endl;
+	/* Call Threads */
+	pthread_create(&inputScannerThread, NULL, &InputScannerController, (void*)&channels);
+	pthread_create(&GarageDoorThread, NULL, &GarageDoorController, (void*)&channels);
+	pthread_create(&keyboardScannerThread, NULL, &KeyboardScannerController, (void*)&channels);
+	/* Join Threads */
+	pthread_join(inputScannerThread, NULL);
+	pthread_join(GarageDoorThread, NULL);
+	pthread_join(keyboardScannerThread, NULL);
 
 	std::cout << "Terminating the Garage Door Simulation" << std::endl;
 	return EXIT_SUCCESS;
